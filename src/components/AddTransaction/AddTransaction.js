7acfe79026b3c26b7711992e9fa5b2b15cdc4f36
Heimdall Sham <heim.sham@icloud.com>
2021-11-15 22:00:47 -0500

    Added transaction

--- src/components/AddTransaction/AddTransaction.js
+++ src/components/AddTransaction/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {GlobalContext} from '../../context/GlobalContext';
+import uniqid from 'uniqid';
 
 function AddTransaction() {
     const [text, setText] = useState('');
-    const [amount, setAmount] = useState(0)
+    const [amount, setAmount] = useState(0);
+
+    const {addTransaction} = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: uniqid(),
+            text, 
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+    }
 
     return (
         <>
             <h3>Add New Transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Text..."></input>
